6f0c6148efccd35a7c17f60fdca7723675ac0b31
Jeremy Stober <stober@gmail.com>
2012-11-01 12:31:24 -0500

    Exposed scatter plot function.

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,6 @@ Description: Common utilities.
 """
 
 from pickle_utils import load_or_compute,loaditer
-from plot_utils import dual_scatter, lvl_scatter
+from plot_utils import dual_scatter, lvl_scatter,scatter
 from utils import create_cluster_colors, create_cluster_colors_rgb, find_duplicates, find_matches, rsme,incavg,consumer, debugflag, timerflag, sp_create, sp_create_data,chunk,flip, sp_create_dict
 from axline import plot_line
